d1f4aa462ccf5451168f2586923e987d9e6c616e
Thiago Araújo <88564332+todearaujo@users.noreply.github.com>
2021-11-05 20:17:50 -0300

    Criação de sobre

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Olá, <b>Tode</b>!</p>"
+
+@app.route("/sobre")
+def sobre():
+    return "<h1>Sobre</h1> <p>Esse site foi criado por <b>Álvaro Justen</b>.</p>"
