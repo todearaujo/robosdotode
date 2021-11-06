38b3a97851a1be143ccde7a3e1c95197cb16bf3f
Thiago Araújo <88564332+todearaujo@users.noreply.github.com>
2021-11-05 22:12:44 -0300

    Primeira tentativa sem HTML

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
 from flask import Flask
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Olá, <b>Tode</b>!</p>"
+    return """
+      <h1>Projeto</h1>
+      <a href="/covid-pr">COVID-19 PR</a> - <a href="/sobre">Sobre esse site</a>
+      <p>
+        Olá, professores e colegas de Master, tudo bem?! Este é o projeto do Tode na disciplina do Turicas do segundo tri.
+      </p>
+    """
 
 @app.route("/sobre")
 def sobre():
-    return "<h1>Sobre</h1> <p>Esse site foi criado por <b>Álvaro Justen</b>.</p>"
+    return """
+      <h1>Sobre</h1>
+      <a href="/">Página inicial</a> - <a href="/covid-pr">COVID-19 PR</a>
+      <p>Esse site foi codado por <b>Álvaro Justen</b> durante uma disciplina do MJDA e editado com códigos Python (um pouco-rs) por Thiago Araújo (Tode). É um projeto contínuo até o fim da disciplina.</p>
+    """
+
+@app.route("/uol-economia")
+linkuol = "https://economia.uol.com.br/" #Link para extração
+respuol = urlopen(linkuol) #Requisição do conteúdo da página
+conteudouol = respuol.read().decode("utf-8") #Definição da variável com o conteúdo
+soupuol = BeautifulSoup(conteudouol, "html.parser") #Chama o análise do BS
+
+for a in soupuol.find('div', {"class": "highlights"}).select('a[href]'): #Encontrar destaques e links
+  print(a.text.strip()) #Imprimir texto
+  print(a['href']) #Imprimir link
+  print() #Dar espaço
+
+def covid_pr():
+    casos, obitos = dados_covid_pr()
+    return f"""
+      <h1>COVID-19: Dados sobre o Paraná</h1>
+      <a href="/">Página inicial</a> - <a href="/sobre">Sobre esse site</a>
+      <p>
+        <b>Casos</b>: {casos}
+      </p>
+      <p>
+        <b>Óbitos</b>: {obitos}
+      </p>
+    """
