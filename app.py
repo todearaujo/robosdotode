b6e4a2c5d4ac57219ac6af99a645c9d3611310e3
Thiago Araújo <88564332+todearaujo@users.noreply.github.com>
2021-11-05 22:40:46 -0300

    Nova tentativa, agora com meu código

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 def hello_world():
     return """
       <h1>Projeto</h1>
-      <a href="/covid-pr">COVID-19 PR</a> - <a href="/sobre">Sobre esse site</a>
+      <a href="/uol-economia">Destaques da home do UOL Economia</a> - <a href="/sobre">Sobre esse site</a>
       <p>
         Olá, professores e colegas de Master, tudo bem?! Este é o projeto do Tode na disciplina do Turicas do segundo tri.
       </p>
@@ -35,13 +35,24 @@ def hello_world():
 def sobre():
     return """
       <h1>Sobre</h1>
-      <a href="/">Página inicial</a> - <a href="/covid-pr">COVID-19 PR</a>
+      <a href="/">Página inicial</a> - <a href="/covid-pr">COVID-19 PR</a> - <a href="/uol-economia">Destaques da home do UOL Economia</a> - 
       <p>Esse site foi codado por <b>Álvaro Justen</b> durante uma disciplina do MJDA e editado com códigos Python (um pouco-rs) por Thiago Araújo (Tode). É um projeto contínuo até o fim da disciplina.</p>
     """
 
 @app.route("/uol-economia")
 
-def covid_pr():
+def uol():
+  linkuol = "https://economia.uol.com.br/" #Link para extração
+  respuol = urlopen(linkuol) #Requisição do conteúdo da página
+  conteudouol = respuol.read().decode("utf-8") #Definição da variável com o conteúdo
+  soupuol = BeautifulSoup(conteudouol, "html.parser") #Chama o análise do BS
+
+  for a in soupuol.find('div', {"class": "highlights"}).select('a[href]'): #Encontrar destaques e links
+    print(a.text.strip()) #Imprimir texto
+    print(a['href']) #Imprimir link
+    print() #Dar espaço
+
+def covid():
     casos, obitos = dados_covid_pr()
     return f"""
       <h1>COVID-19: Dados sobre o Paraná</h1>
