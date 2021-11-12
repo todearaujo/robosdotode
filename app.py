83332723052ea6eb1e794f33750cac7b46c129c3
Thiago Araújo <88564332+todearaujo@users.noreply.github.com>
2021-11-12 20:58:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,25 @@ import requests
 
 app = Flask(__name__)
 
+def dados_covid_pr():
+    hoje = datetime.datetime.now().date()
+    ontem = hoje - datetime.timedelta(days=1)
+    for data in (hoje, ontem):
+        url = f"https://www.saude.pr.gov.br/sites/default/arquivos_restritos/files/documento/{data.year}-{data.month:02d}/INFORME_EPIDEMIOLOGICO_{data.day:02d}_{data.month:02d}_{data.year}_OBITOS_CASOS_Municipio.csv"
+        resposta = requests.get(url)
+        if resposta.ok:
+            conteudo = resposta.content.decode(resposta.apparent_encoding)
+            break
+  
+    casos = 0
+    obitos = 0
+    leitor = csv.DictReader(io.StringIO(conteudo), delimiter=";")
+    for registro in leitor:
+        casos += int(registro["Casos"])
+        obitos += int(registro["Obitos"])
+
+    return data, casos, obitos
+
 @app.route("/")
 def hello_world():
     return render_template(
@@ -14,5 +33,10 @@ def hello_world():
 
 @app.route("/sobre")
 def sobre():
+    arquivo = open("templates/sobre.html")
+    return arquivo.read()
+
+@app.route("/covid-pr")
+def covid():
     arquivo = open("templates/sobre.html")
     return arquivo.read()
