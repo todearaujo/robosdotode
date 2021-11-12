161f6f1ad348e1c72c06d8e7b5d6ff84ce426fed
Thiago Araújo <88564332+todearaujo@users.noreply.github.com>
2021-11-12 20:32:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,33 +5,14 @@ import csv
 import io
 import requests
 
-def dados_covid_pr():
-    url = "https://www.saude.pr.gov.br/sites/default/arquivos_restritos/files/documento/2021-11/INFORME_EPIDEMIOLOGICO_05_11_2021_OBITOS_CASOS_Municipio.csv"
-    resposta = requests.get(url)
-    conteudo = resposta.content.decode(resposta.apparent_encoding)
-  
-    casos = 0
-    obitos = 0
-    leitor = csv.DictReader(io.StringIO(conteudo), delimiter=";")
-    for registro in leitor:
-        casos += int(registro["Casos"])
-        obitos += int(registro["Obitos"])
-
-    return casos, obitos
-
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return """
-      <h1>Projeto</h1>
-      <a href="/uol-economia">Destaques da home do UOL Economia</a> - <a href="/sobre">Sobre esse site</a>
-      <p>
-        Olá, professores e colegas de Master, tudo bem?! Este é o projeto do Tode na disciplina do Turicas do segundo tri.
-      </p>
-    """
+    arquivo = open("templates/home.html")
+    return arquivo.read()
 
 @app.route("/sobre")
 def sobre():
-    arquivo = open("templates/home.html")
+    arquivo = open("templates/sobre.html")
     return arquivo.read()
