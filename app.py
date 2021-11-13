f3453753dd30240cccfd792855f2c40da38e2a77
Thiago Araújo <88564332+todearaujo@users.noreply.github.com>
2021-11-12 21:59:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
-from flask import Flask
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
 import csv
+import datetime
 import io
 import requests
+from flask import Flask, render_template
+
+def dados_covid_pr():
+    hoje = datetime.datetime.now().date()
+    ontem = hoje - datetime.timedelta(days=1)
+    conteudo = None
+    for data in (hoje, ontem):
+        url1 = f"https://www.saude.pr.gov.br/sites/default/arquivos_restritos/files/documento/{data.year}-{data.month:02d}/INFORME_EPIDEMIOLOGICO_{data.day:02d}_{data.month:02d}_{data.year}_OBITOS_CASOS_Municipio.csv"
+        url2 = url1.lower()
+        for url in (url1, url2):
+            resposta = requests.get(url)
+            if resposta.ok:
+                conteudo = resposta.content.decode(resposta.apparent_encoding)
+                break
+        if conteudo:
+            break
+    if not conteudo:
+        return None, None, None
+    casos = 0
+    obitos = 0
+    leitor = csv.DictReader(io.StringIO(conteudo), delimiter=";")
+    for registro in leitor:
+        casos += int(registro["Casos"])
+        obitos += int(registro["Obitos"])
+
+    return data, casos, obitos
 
 app = Flask(__name__)
 
@@ -17,7 +41,7 @@ def sobre():
     arquivo = open("templates/sobre.html")
     return arquivo.read()
 
-@app.route("/covid-pr")
+@app.route("/covid-19")
 def covid():
-    arquivo = open("templates/covid-pr.html")
+    arquivo = open("templates/covid-19.html")
     return arquivo.read()
