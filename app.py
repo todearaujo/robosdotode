d6f29c3cccdc3e4670bd146c0c13dbfdde8fef27
Thiago Araújo <88564332+todearaujo@users.noreply.github.com>
2021-11-12 21:50:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,27 @@ from flask import Flask
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import csv
+import io
+import requests
 
 app = Flask(__name__)
 
 def dados_covid_pr():
     hoje = datetime.datetime.now().date()
     ontem = hoje - datetime.timedelta(days=1)
+    conteudo = None
     for data in (hoje, ontem):
-        url = f"https://www.saude.pr.gov.br/sites/default/arquivos_restritos/files/documento/{data.year}-{data.month:02d}/INFORME_EPIDEMIOLOGICO_{data.day:02d}_{data.month:02d}_{data.year}_OBITOS_CASOS_Municipio.csv"
-        resposta = requests.get(url)
-        if resposta.ok:
-            conteudo = resposta.content.decode(resposta.apparent_encoding)
+        url1 = f"https://www.saude.pr.gov.br/sites/default/arquivos_restritos/files/documento/{data.year}-{data.month:02d}/INFORME_EPIDEMIOLOGICO_{data.day:02d}_{data.month:02d}_{data.year}_OBITOS_CASOS_Municipio.csv"
+        url2 = url1.lower()
+        for url in (url1, url2):
+            resposta = requests.get(url)
+            if resposta.ok:
+                conteudo = resposta.content.decode(resposta.apparent_encoding)
+                break
+        if conteudo:
             break
-  
+    if not conteudo:
+        return None, None, None
     casos = 0
     obitos = 0
     leitor = csv.DictReader(io.StringIO(conteudo), delimiter=";")
@@ -25,5 +33,20 @@ def dados_covid_pr():
     return data, casos, obitos
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def hello_world():
+    return render_template(
+        "home.html")
+
+@app.route("/sobre")
+def sobre():
+    arquivo = open("templates/sobre.html")
+    return arquivo.read()
+
+@app.route("/covid-19")
+def covid():
+    ultima_data, casos, obitos = dados_covid_pr()
+    return render_template("covid-19.html",
+                           data=ultima_data,
+                           casos=casos_pr,
+                           obitos=obitos_pr
+    )
