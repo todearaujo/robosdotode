5951af37e887c1cb246ea1c97fbe273f2e901599
Thiago Araújo <github@todearaujo.com>
2022-01-01 01:48:08 -0300

    Novo serviceworker

--- templates/sw.js
+++ templates/sw.js
@@ -1,61 +1,48 @@
-// simple pwa config
-var cacheVersion = new Date().getTime();
-var staticCacheName = "flask-pwa-v" + cacheVersion
+console.log('Hello from sw.js');
 
-var filesToCache = [
-    '/',
-    '/sobre',
-    '/offline',
-    '/static/48.png',
-    '/static/72.png',
-    '/static/96.png',
-    '/static/128.png',
-    '/static/192.png',
-    '/static/384.png',
-    '/static/512.png',
-    '/static/favicon-16x16.png',
-    '/static/favicon-32x32.png',
-    '/static/favicon.ico',
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js');
 
-// Cache on install
-self.addEventListener("install", event => {
-  this.skipWaiting();
-  event.waitUntil(
-    caches.open(staticCacheName)
-      .then(cache => {
-        return cache.addAll(filesToCache);
-      })
-  )
-});
+if (workbox) {
+  console.log("Yay! Workbox is loaded 🎉");
 
-// Clear cache on activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => (cacheName.startsWith("flask-pwa-")))
-          .filter(cacheName => (cacheName !== staticCacheName))
-          .map(cacheName => caches.delete(cacheName))
-      );
-    })
+  workbox.precaching.precacheAndRoute([
+    {
+      "url": "/",
+      "revision": "1"
+    }
+  ]);
+
+  workbox.routing.registerRoute(
+    /\.(?:js|css)$/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'static-resources',
+    }),
   );
-});
 
-// Serve from cache, and return offline page if client is offline 
-this.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-    event.respondWith(
-      fetch(event.request.url).catch(error => {
-        return caches.match('/offline');
-      })
-    );
-  } else{
-    event.respondWith(caches.match(event.request)
-        .then(function (response) {
-        return response || fetch(event.request);
-      })
-    );
-  }
-});
+  workbox.routing.registerRoute(
+    /\.(?:png|gif|jpg|jpeg|svg)$/,
+    workbox.strategies.cacheFirst({
+      cacheName: 'images',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 60,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+        }),
+      ],
+    }),
+  );
+
+  workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'googleapis',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30,
+        }),
+      ],
+    }),
+  );
+} else {
+  console.log("Boo! Workbox didn't load 😬");
+}
